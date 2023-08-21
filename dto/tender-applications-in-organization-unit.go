aefb60f5e1a6bc43c82ac6597d18f81942fcf45b
oykos-development-hub <hub@oykos.me>
2023-08-21 17:20:26 +0000

    Update code from GitLab

--- dto/tender-applications-in-organization-unit.go
+++ dto/tender-applications-in-organization-unit.go
@@ -6,41 +6,91 @@ import (
 	"gitlab.sudovi.me/erp/hr-ms-api/data"
 )
 
+type TenderApplicationType string
+
+const (
+	Internal TenderApplicationType = "internal"
+	External TenderApplicationType = "external"
+)
+
 type TenderApplicationsInOrganizationUnitDTO struct {
-	JobTenderID   int  `json:"job_tender_id" validate:"required"`
-	UserProfileID int  `json:"user_profile_id" validate:"required"`
-	Active        bool `json:"active" validate:"required"`
-	FileID        *int `json:"file_id"`
+	JobTenderID        int                   `json:"job_tender_id" validate:"required"`
+	UserProfileID      *int                  `json:"user_profile_id"`
+	Active             bool                  `json:"active" validate:"required"`
+	Type               TenderApplicationType `json:"type" validate:"required"`
+	FirstName          *string               `json:"first_name"`
+	LastName           *string               `json:"last_name"`
+	Nationality        *string               `json:"nationality"`
+	DateOfBirth        *string               `json:"date_of_birth"`
+	DateOfApplication  string                `json:"date_of_application" validate:"required"`
+	OfficialPersonalID *string               `json:"official_personal_id"`
+	Evaluation         *string               `json:"evaluation"`
+	Status             string                `json:"status" validate:"required"`
+	FileID             *int                  `json:"file_id"`
 }
 
 type TenderApplicationsInOrganizationUnitResponseDTO struct {
-	ID            int       `json:"id"`
-	JobTenderID   int       `json:"job_tender_id"`
-	UserProfileID int       `json:"user_profile_id"`
-	Active        bool      `json:"active"`
-	FileID        *int      `json:"file_id"`
-	CreatedAt     time.Time `json:"created_at"`
-	UpdatedAt     time.Time `json:"updated_at"`
+	ID                 int                   `json:"id"`
+	JobTenderID        int                   `json:"job_tender_id"`
+	UserProfileID      *int                  `json:"user_profile_id"`
+	Active             bool                  `json:"active"`
+	Type               TenderApplicationType `json:"type"`
+	FirstName          *string               `json:"first_name"`
+	LastName           *string               `json:"last_name"`
+	Nationality        *string               `json:"nationality"`
+	DateOfBirth        *string               `json:"date_of_birth"`
+	DateOfApplication  string                `json:"date_of_application"`
+	OfficialPersonalID *string               `json:"official_personal_id"`
+	Evaluation         *string               `json:"evaluation"`
+	Status             string                `json:"status"`
+	FileID             *int                  `json:"file_id"`
+	CreatedAt          time.Time             `json:"created_at"`
+	UpdatedAt          time.Time             `json:"updated_at"`
 }
 
 func (dto TenderApplicationsInOrganizationUnitDTO) ToTenderApplicationsInOrganizationUnit() *data.TenderApplicationsInOrganizationUnit {
 	return &data.TenderApplicationsInOrganizationUnit{
-		JobTenderID:   dto.JobTenderID,
-		UserProfileID: dto.UserProfileID,
-		Active:        dto.Active,
-		FileID:        dto.FileID,
+		JobTenderID:        dto.JobTenderID,
+		UserProfileID:      dto.UserProfileID,
+		IsInternal:         dto.Type == Internal,
+		Active:             dto.Active,
+		FirstName:          dto.FirstName,
+		LastName:           dto.LastName,
+		Status:             dto.Status,
+		Evaluation:         dto.Evaluation,
+		OfficialPersonalID: dto.OfficialPersonalID,
+		DateOfBirth:        dto.DateOfBirth,
+		Nationality:        dto.Nationality,
+		DateOfApplication:  dto.DateOfApplication,
+		FileID:             dto.FileID,
 	}
 }
 
 func ToTenderApplicationsInOrganizationUnitResponseDTO(data data.TenderApplicationsInOrganizationUnit) TenderApplicationsInOrganizationUnitResponseDTO {
+	var applicationType TenderApplicationType
+	if data.IsInternal {
+		applicationType = Internal
+	} else {
+		applicationType = External
+	}
+
 	return TenderApplicationsInOrganizationUnitResponseDTO{
-		ID:            data.ID,
-		JobTenderID:   data.JobTenderID,
-		UserProfileID: data.UserProfileID,
-		Active:        data.Active,
-		FileID:        data.FileID,
-		CreatedAt:     data.CreatedAt,
-		UpdatedAt:     data.UpdatedAt,
+		ID:                 data.ID,
+		JobTenderID:        data.JobTenderID,
+		UserProfileID:      data.UserProfileID,
+		Active:             data.Active,
+		Type:               applicationType,
+		FirstName:          data.FirstName,
+		LastName:           data.LastName,
+		Status:             data.Status,
+		Evaluation:         data.Evaluation,
+		OfficialPersonalID: data.OfficialPersonalID,
+		DateOfBirth:        data.DateOfBirth,
+		DateOfApplication:  data.DateOfApplication,
+		FileID:             data.FileID,
+		Nationality:        data.Nationality,
+		CreatedAt:          data.CreatedAt,
+		UpdatedAt:          data.UpdatedAt,
 	}
 }
 
