a7757aa180f0cb02ab838c4431e53ad337cd9364
oykos-development-hub <hub@oykos.me>
2023-10-16 14:33:02 +0000

    Update code from GitLab

--- migrations/1695115175875363_revisions.postgres.up.sql
+++ migrations/1695115175875363_revisions.postgres.up.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS revisions (
     serial_number TEXT NOT NULL,
     date_of_revision DATE NOT NULL,
     revision_quartal TEXT NOT NULL,
-    internal_revision_subject INTEGER[],
+    internal_revision_subject_id INTEGER[],
     external_revision_subject INTEGER,
     revisor_id INTEGER[] NOT NULL REFERENCES user_profiles(id) ON DELETE CASCADE,
     revision_type_id INTEGER NOT NULL,
