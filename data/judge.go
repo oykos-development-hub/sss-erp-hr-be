71f3aa9e4ecabfa699484ce592e95b2d2c538a35
oykos-development-hub <hub@oykos.me>
2024-03-14 15:38:20 +0000

    Update code from GitLab

--- data/judge.go
+++ data/judge.go
@@ -44,7 +44,7 @@ func (t *Judge) GetAll(page *int, size *int, condition *up.Cond) ([]*Judge, *uin
 		res = paginateResult(res, *page, *size)
 	}
 
-	err = res.All(&all)
+	err = res.OrderBy("created_at desc").All(&all)
 	if err != nil {
 		return nil, nil, err
 	}
