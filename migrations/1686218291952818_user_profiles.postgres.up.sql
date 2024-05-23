f9060a70c947f70d9303d59c43dd18bd705f66f7
oykos-development-hub <hub@oykos.me>
2024-05-23 12:33:47 +0000

    Update code from GitLab

--- migrations/1686218291952818_user_profiles.postgres.up.sql
+++ migrations/1686218291952818_user_profiles.postgres.up.sql
@@ -38,3 +38,17 @@ CREATE TABLE user_profiles (
     created_at timestamp without time zone NOT NULL DEFAULT now(),
     updated_at timestamp without time zone NOT NULL DEFAULT now()
 );
+
+INSERT INTO user_profiles (
+    user_account_id, first_name, middle_name, last_name, birth_last_name, father_name, mother_name, mother_birth_last_name,
+    date_of_birth, country_of_birth, city_of_birth, nationality, national_minority, citizenship, address, bank_account, 
+    bank_name, official_personal_id, official_personal_document_number, official_personal_document_issuer, gender, 
+    single_parent, housing_done, housing_description, martial_status, date_of_taking_oath, date_of_becoming_judge, 
+    active_contract, revisor_role, engagement_type_id, created_at, updated_at, personal_id, is_judge, file_id, year, 
+    judge_application_submission_date
+) VALUES (
+    1, 'Nikola', '', 'Perović', '', 'Marko', 'Mila', '', '1990-05-05', 'mne', 'Podgorica', '', '', 'mne', 
+    'Ulica Vladana Giljena 21', '', '', '0505990191919', 'DOC67890', 'PJ Bar', 'M', 'f', 't', 'Stan u Nikšiću', 
+    'Oženjen', '2023-10-26', NULL, NULL, NULL, NULL, '2023-10-26 08:30:48.534364', '2023-10-26 08:30:48.534366', '', 'f', 
+    0, 2024, NULL
+);
